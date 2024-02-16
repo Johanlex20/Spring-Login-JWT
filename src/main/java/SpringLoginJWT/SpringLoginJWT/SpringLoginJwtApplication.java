54fb6d2727780d43cd4f24a5973772ba34ac1a8f
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-02-16 11:26:45 -0500

    Mensaje de confirmación

---
+++ src/main/java/SpringLoginJWT/SpringLoginJWT/SpringLoginJwtApplication.java
@@ -0,0 +1,13 @@
+package SpringLoginJWT.SpringLoginJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringLoginJWT/SpringLoginJWT/SpringLoginJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringLoginJWT.SpringLoginJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
